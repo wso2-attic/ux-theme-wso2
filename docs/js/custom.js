f5af3169f394d544eae259c2bf27215cf72ffa75
thusithakalugamage <thusitha32@yahoo.com>
2016-03-10 15:04:59 +0530

    adding map component to the theme

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -90,10 +90,43 @@ $('link[data-include-demo]').each(function(){
 });
 
 
+
+    /***********************************************************
+     *  accordion config
+     ***********************************************************/
+
 $('#accordion1').on('shown.bs.collapse', function (e,f) {
     var elem = $(this).children().children('.in');
 
     if($(elem).hasClass('in')){
         $(elem).prev().find('input[type=radio]').prop('checked',true);
     }
+});
+
+
+
+    /***********************************************************
+     *  leafletjs map config
+     ***********************************************************/
+
+$(function(){
+    var lat     = $(".map-container").data("lat"),
+        long    = $(".map-container").data("long"),
+        container = 'map1',
+        zoomLevel = 17,
+        tileSet = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        attrib =  '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+        if(lat != null && lat != undefined && lat != "" && long != null && long != undefined && long != "") {
+            $(".map-error").hide();
+            $(".map-container").show();
+        }else{
+            $(".map-container").hide();
+            $(".map-error").show();
+        }
+
+    var map = L.map(container).setView([lat,long], zoomLevel);
+
+    L.tileLayer(tileSet, { attribution: attrib}).addTo(map);
+    L.marker([lat,long]).addTo(map).bindPopup('Your are here..!').openPopup();
 });
