c5c05666dc953c562d633fdf4bc6b490854bc409
Imesh Chandrasiri <imeshc@wso2.com>
2016-06-09 15:12:32 +0530

    Added Copy to clipboard libray

--- js/functions.js
+++ js/functions.js
@@ -642,6 +642,30 @@ $.fn.collapse_nav_sub = function(){
     }
 };
 
+
+/**
+ * Copy to clipboard function using ZeroClipboard plugin
+ * @return object
+ */
+$.fn.zclip = function() {
+
+    if(typeof ZeroClipboard == 'function'){
+        var client = new ZeroClipboard( this );
+        client.on( "ready", function( readyEvent ) {
+          client.on( "aftercopy", function( event ) {
+            var target = $(event.target);
+            target.attr("title","Copied!")
+            target.tooltip('enable');
+            target.tooltip("show");
+            target.tooltip('disable');
+          });
+        });
+    }else{
+        console.warn('Warning : Dependency missing - ZeroClipboard Library');
+    }
+    return this;
+};
+
 $('.sidebar-wrapper[data-fixed-offset-top]').on('affix.bs.affix', function() {
     $(this).css('top', $(this).data('fixed-offset-top'));
 });
