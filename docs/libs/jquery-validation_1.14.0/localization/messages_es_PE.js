ee1ea2972eaa04d72965fa3c366376ffd3a46b88
Jerad Rutnam <jeradrutnam@outlook.com>
2017-01-16 13:40:13 +0530

    Updated jquery validation lib

--- docs/libs/jquery-validation_1.14.0/localization/messages_es_PE.js
+++ docs/libs/jquery-validation_1.14.0/localization/messages_es_PE.js
@@ -1,6 +1,8 @@
 (function( factory ) {
 	if ( typeof define === "function" && define.amd ) {
 		define( ["jquery", "../jquery.validate"], factory );
+	} else if (typeof module === "object" && module.exports) {
+		module.exports = factory( require( "jquery" ) );
 	} else {
 		factory( jQuery );
 	}
@@ -11,7 +13,7 @@
  * Locale: ES (Spanish; Español)
  * Region: PE (Perú)
  */
-$.extend($.validator.messages, {
+$.extend( $.validator.messages, {
 	required: "Este campo es obligatorio.",
 	remote: "Por favor, llene este campo.",
 	email: "Por favor, escriba un correo electrónico válido.",
@@ -23,15 +25,15 @@ $.extend($.validator.messages, {
 	creditcard: "Por favor, escriba un número de tarjeta válido.",
 	equalTo: "Por favor, escriba el mismo valor de nuevo.",
 	extension: "Por favor, escriba un valor con una extensión permitida.",
-	maxlength: $.validator.format("Por favor, no escriba más de {0} caracteres."),
-	minlength: $.validator.format("Por favor, no escriba menos de {0} caracteres."),
-	rangelength: $.validator.format("Por favor, escriba un valor entre {0} y {1} caracteres."),
-	range: $.validator.format("Por favor, escriba un valor entre {0} y {1}."),
-	max: $.validator.format("Por favor, escriba un valor menor o igual a {0}."),
-	min: $.validator.format("Por favor, escriba un valor mayor o igual a {0}."),
+	maxlength: $.validator.format( "Por favor, no escriba más de {0} caracteres." ),
+	minlength: $.validator.format( "Por favor, no escriba menos de {0} caracteres." ),
+	rangelength: $.validator.format( "Por favor, escriba un valor entre {0} y {1} caracteres." ),
+	range: $.validator.format( "Por favor, escriba un valor entre {0} y {1}." ),
+	max: $.validator.format( "Por favor, escriba un valor menor o igual a {0}." ),
+	min: $.validator.format( "Por favor, escriba un valor mayor o igual a {0}." ),
 	nifES: "Por favor, escriba un NIF válido.",
 	nieES: "Por favor, escriba un NIE válido.",
 	cifES: "Por favor, escriba un CIF válido."
-});
+} );
 
 }));
--- docs/libs/jquery-validation_1.14.0/localization/messages_ge.js
+++ docs/libs/jquery-validation_1.14.0/localization/messages_ge.js
@@ -1,6 +1,8 @@
 (function( factory ) {
 	if ( typeof define === "function" && define.amd ) {
 		define( ["jquery", "../jquery.validate"], factory );
+	} else if (typeof module === "object" && module.exports) {
+		module.exports = factory( require( "jquery" ) );
 	} else {
 		factory( jQuery );
 	}
@@ -11,13 +13,13 @@
  * Translated default messages for the jQuery validation plugin.
  * Locale: GE (Georgian; ქართული)
  */
-$.extend($.validator.messages, {
+$.extend( $.validator.messages, {
 	required: "ეს ველი სავალდებულოა",
 	remote: "გთხოვთ შეასწოროთ.",
 	email: "გთხოვთ შეიყვანოთ სწორი ფორმატით.",
 	url: "გთხოვთ შეიყვანოთ სწორი ფორმატით.",
 	date: "გთხოვთ შეიყვანოთ სწორი თარიღი.",
-	dateISO: "გთხოვთ შეიყვანოთ სწორი ფორმატით ( ISO ).",
+	dateISO: "გთხოვთ შეიყვანოთ სწორი ფორმატით (ISO).",
 	number: "გთხოვთ შეიყვანოთ რიცხვი.",
 	digits: "დაშვებულია მხოლოდ ციფრები.",
 	creditcard: "გთხოვთ შეიყვანოთ სწორი ფორმატის ბარათის კოდი.",
@@ -28,6 +30,6 @@ $.extend($.validator.messages, {
 	range: $.validator.format( "შეიყვანეთ {0} -სა {1} -ს შორის." ),
 	max: $.validator.format( "გთხოვთ შეიყვანოთ მნიშვნელობა ნაკლები ან ტოლი {0} -ს." ),
 	min: $.validator.format( "გთხოვთ შეიყვანოთ მნიშვნელობა მეტი ან ტოლი {0} -ს." )
-});
+} );
 
 }));
