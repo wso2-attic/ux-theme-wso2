dee28c338f10121715d90096d15dd2845c503e80
jeradrutnam <jeradrutnam@outlook.com>
2017-09-25 08:48:03 +0530

    Fixed bootstrap loading order issue

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,45 @@
 import React from 'react';
 import {render} from 'react-dom';
-import { Button } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Alert } from 'reactstrap';
 
 class App extends React.Component {
     
-    constructor(){
-        super();
+    constructor(props) {
+        super(props);
+
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    toggleNavbar() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
     }
     
     render() {
         return (
-            <Button color="danger">Danger!</Button>
+            <div>
+                <Navbar color="faded" light expand>
+                    <NavbarToggler right onClick={this.toggle} />
+                    <NavbarBrand href="/">Theme WSO2</NavbarBrand>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/components/">Components</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="https://github.com/wso2-dev-ux/theme-wso2" target="_blank">Github</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
         );
     }
+    
 }
 
 render(<App/>, document.getElementById('app'));
