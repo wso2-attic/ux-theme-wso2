0b60b1cf890a4c5d0b9eaa11626018037ed321d5
jeradrutnam <jeradrutnam@outlook.com>
2017-10-23 00:00:14 +0530

    Added eslint & sasslint watch support

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ class App extends React.Component {
                     <h1 className="display-3">Hello, world!</h1>
                     <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
                     <hr className="my-2" />
-                    <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.
+                    <p>It uses utility classes for typgraphy and spacing to space contentout within the larger container.
                     </p>
                     <p className="lead">
                         <Button color="primary">Learn More</Button>
