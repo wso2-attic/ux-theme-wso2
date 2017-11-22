648b8fd6df6071f4bf9fc1ec87085d2382324d05
jeradrutnam <jeradrutnam@outlook.com>
2017-11-22 17:15:16 +0530

    Added class extending support to Button component

--- module/components/Button/index.js
+++ module/components/Button/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import classNames from 'classnames';
 import { Button as ReactstrapButton } from 'reactstrap';
 
 export class Button extends React.Component {
    render () {
-       return (
-          <ReactstrapButton color="primary" size="lg">{ this.props.children }</ReactstrapButton>
-       );
+        const { classes,
+               component,
+               dense,
+               disableFocusRipple,
+               disableRipple,
+               disabled,
+               fab,
+               href,
+               raised 
+             } = this.props;
+       
+        const Component = component || 'button';
+       
+        let defaultClasses = '';
+       
+        return (
+            <ReactstrapButton className={ classNames(classes, defaultClasses) }>{ this.props.children }</ReactstrapButton>
+        );
    }
 }
