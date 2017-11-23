93d62d86b572a34b5dc9aee0a5aaab0522748f3b
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2017-11-23 12:30:29 +0530

    added other prop types

--- module/components/Button/index.js
+++ module/components/Button/index.js
@@ -7,11 +7,55 @@ const propTypes = {
     /**
      * Useful to extend the style applied to components.
      */
-    classes: PropTypes.Object,
+    classes: PropTypes.object,
+    /**
+     * The component used for the root node.
+     * Either a string to use a DOM element or a component.
+     * The default value is a `button`.
+     */
+    component: PropTypes.element,
+    /**
+     * Uses a smaller minWidth, ideal for things like card actions.
+     */
+    dense: PropTypes.bool,
+    /**
+     * If `true`, the  keyboard focus ripple will be disabled.
+     * `disableRipple` must also be true.
+     */
+    disableFocusRipple:  PropTypes.bool,
+    /**
+     * If `true`, the ripple effect will be disabled.
+     */
+    disableRipple: PropTypes.bool,
+    /**
+     * If `true`, the button will be disabled.
+     */
+    disabled: PropTypes.bool,
+    /**
+     * If `true`, will use floating action button styling.
+     */
+    fab: PropTypes.bool,
+    /**
+     * The URL to link to when the button is clicked.
+     * If defined, an `a` element will be used as the root node.
+     */
+    href: PropTypes.string,
+    /**
+     * If `true`, the button will use raised styling.
+     */
+    raised: PropTypes.bool,
     /**
      * The color of the component. It's using the theme palette when that makes sense.
      */
     color: PropTypes.string,
+    /**
+     * If `true`, the button will use outline styling.
+     */
+    outline: PropTypes.bool,
+    /**
+     * Supports for large or small button
+     */
+    size: PropTypes.oneOf(['sm', 'lg']),
 };
 
 /**
