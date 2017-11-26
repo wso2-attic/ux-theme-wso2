4009cce0882327203219cd6a20ee96a7992972f1
Imesh Chandrasiri <dimal.chandrasiri@gmail.com>
2017-11-26 15:58:43 +0530

    Adding updated chip component

--- module/components/Chip/Readme.md
+++ module/components/Chip/Readme.md
@@ -1,5 +1,12 @@
 Chip example:
 
 ```js
-<Chip labelText="This is a sample chip" onClick={function(){console.log('This is a sample click event')}}/>
+<div>
+  <Chip label="This is a sample chip">
+    <Avatar backgroundColor="#333" size="100" />
+    
+  </Chip>
+  <Chip label="This is a sample chip" onClick={function(){console.log('This is a icon click')}} onRequestDelete={function(){console.log('This is a sample click event')}}/>
+  <Chip label="This is a sample chip" onClick={function(){console.log('This is a sample click event')}}/>
+</div>
 ```
--- module/components/Chip/index.js
+++ module/components/Chip/index.js
@@ -3,49 +3,95 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 const propTypes = {
-	classes: PropTypes.object,
-	backgroundColor: PropTypes.string,
-	labelText: PropTypes.string,
-	onClick: PropTypes.func,
+	/*
+	 * Used to extend default object classes
+	 */
+	classes : PropTypes.object,
+	/*
+	 * Click event handler
+	 */
+	onClick : PropTypes.func,
+	/*
+	 * Delete event handler
+	 */
+	onRequestDelete : PropTypes.func,
+	/*
+	 * This property will contain label text
+	 */
+	label : PropTypes.string,
+	/*
+	 * This property will contain label styling
+	 */
+	labelStyle : PropTypes.object
 }
 
-export default class Chip extends React.Component {
+class Chip extends React.Component{
+
 	constructor(props){
 		super(props);
-
 		this.onClick = this.onClick.bind(this);
+		this.handleDeleteIconClick = this.handleDeleteIconClick.bind(this);
 	}
 
 	onClick(e) {
-	    if (this.props.disabled) {
-	      e.preventDefault();
-	      return;
+		if (this.props.onClick) {
+		  this.props.onClick(e);
+		}
+	}
+
+	handleDeleteIconClick(e){
+	    e.stopPropagation();
+
+	    if (this.props.onRequestDelete) {
+	      this.props.onRequestDelete(event);
 	    }
-
-	    if (this.props.onClick) {
-	      	this.props.onClick(e);
-    	}
-
-  	}
+  	};
 
 	render(){
 
-		let {
-	      classes,
-	      backgroundColor,
-	      labelText,
-	      onClick,
-	    } = this.props;
+		let defaultClasses = 'chips chips-rounded',
+			defaultLabelClasses = 'chips-label',
+			avatar = null,
+			deleteIcon = '';
 
-	    let defaultClasses = 'chips chips-rounded';
+		const {
+			classes,
+			children: childrenProp,
+			onClick,
+			label,
+			onRequestDelete,
+			labelStyle,
+		} = this.props;
+
+		let children = childrenProp;
+		const childrenCount = React.Children.count(children);
+
+		if (childrenCount >= 1) {
+	      children = React.Children.toArray(children);
+
+	      if (React.isValidElement(children[0]) && children[0].type.name === 'Avatar') {
+	        avatar = children.shift();
+	        avatar = React.cloneElement(avatar);
+	      }
+	    }
+		
+		if(onRequestDelete){
+			deleteIcon = (
+				<div className="deleteIcon" onClick={this.handleDeleteIconClick} />
+			);
+		}
 
 		return (
-			<div className={classNames(classes, defaultClasses)} onClick={ this.onClick }>
-				<span>{this.props.labelText}</span>
-			</div>
+			<div className={classNames(classes, defaultClasses)} onClick={onClick} >
+				{avatar}
+		        <span className={classNames(labelStyle, defaultLabelClasses)}>{label}</span>
+		        {deleteIcon}
+	      	</div>
 		)
 	}
 
 }
 
-Chip.propTypes = propTypes;
+Chip.PropTypes = propTypes;
+
+export default Chip;
