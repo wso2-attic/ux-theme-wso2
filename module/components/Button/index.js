4037d66afd6c4b280052fd0cbfdbcd4479252cfe
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2017-11-27 12:50:54 +0530

    Add eslint compatible file

--- module/components/Button/index.js
+++ module/components/Button/index.js
@@ -9,37 +9,13 @@ const propTypes = {
      */
     classes: PropTypes.string,
     /**
-     * The component used for the root node.
-     * Either a string to use a DOM element or a component.
-     * The default value is a `button`.
-     */
-    component: PropTypes.element,
-    /**
      * Uses a smaller minWidth, ideal for things like card actions.
      */
     dense: PropTypes.bool,
     /**
-     * If `true`, the  keyboard focus ripple will be disabled.
-     * `disableRipple` must also be true.
-     */
-    disableFocusRipple:  PropTypes.bool,
-    /**
-     * If `true`, the ripple effect will be disabled.
-     */
-    disableRipple: PropTypes.bool,
-    /**
      * If `true`, the button will be disabled.
      */
     disabled: PropTypes.bool,
-    /**
-     * If `true`, will use floating action button styling.
-     */
-    fab: PropTypes.bool,
-    /**
-     * The URL to link to when the button is clicked.
-     * If defined, an `a` element will be used as the root node.
-     */
-    href: PropTypes.string,
     /**
      * If `true`, the button will use raised styling.
      */
@@ -56,49 +32,59 @@ const propTypes = {
      * Supports for large or small button
      */
     size: PropTypes.oneOf(['sm', 'lg', '']),
+    /**
+     * @ignore
+     */
+    children: PropTypes.node,
 };
 
 const defaultProps = {
     size: '',
+    color: 'default',
     outline: false,
     disabled: false,
     raised: false,
-    dense: false
+    dense: false,
 };
 
 /**
  * Default button
  */
 export class Button extends React.Component {
-   constructor(props) {
+    constructor(props) {
         super(props);
-   }
-    
-   render () {
-        const { classes,
-               component,
-               dense,
-               disableFocusRipple,
-               disableRipple,
-               disabled,
-               fab,
-               href,
-               raised,
-               color,
-               outline,
-               size
-             } = this.props;
-       
-        let materialClasses = [
-                                raised ? `raised` : false ,
-                                dense ? `dense` : false,
-                                ];
+
+        this.onClick = this.onClick.bind(this);
+    }
+
+    render() {
+        const {
+            classes,
+            dense,
+            disabled,
+            raised,
+            color,
+            outline,
+            size,
+        } = this.props;
+
+        const materialClasses = [
+            raised ? 'raised' : false,
+            dense ? 'dense' : false,
+        ];
 
         return (
-            <ReactstrapButton className={ classNames(classes, materialClasses) } color={ color } outline={ outline }
-                              disabled={ disabled } size={ size }>{ this.props.children }</ReactstrapButton>
+            <ReactstrapButton
+                className={classNames(classes, materialClasses)}
+                color={color}
+                outline={outline}
+                disabled={disabled}
+                size={size}
+            >
+                { this.props.children }
+            </ReactstrapButton>
         );
-   }
+    }
 }
 
 Button.propTypes = propTypes;
