91a052074e2f53768a2f02f0d31f7d5acb3f31a7
thusithakalugamage <thusitha32@yahoo.com>
2017-12-04 14:31:56 +0530

    adding styleguide configs

--- styleguide/setup.js
+++ styleguide/setup.js
@@ -1,2 +1,4 @@
 import Toolbar from '../module/components/AppBar/Toolbar';
+import CardHeader from '../module/components/Card/CardHeader';
 global.Toolbar = Toolbar;
+global.CardHeader = CardHeader;
